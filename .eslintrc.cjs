3b1f25eebbaca1f56154036d2cd7201e8ff4b996
ChunkyProgrammer <78101139+ChunkyProgrammer@users.noreply.github.com>
2023-01-23 14:22:55 -0500

    improve linting of files

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  // https://eslint.org/docs/user-guide/configuring#using-configuration-files-1
+  root: true,
+  extends: [ 'eslint:recommended', 'plugin:unicorn/recommended', 'plugin:sonarjs/recommended', 'plugin:jsdoc/recommended', 'plugin:n/recommended', 'plugin:import/recommended'],
+  plugins: ['unicorn'],
+  env: {
+    browser: true,
+    commonjs: true,
+    es2022: true,
+    node: true
+  },
+
+  parserOptions: {
+    ecmaVersion: 2022,
+    impliedStrict: true,
+    sourceType: 'module'
+  },
+
+  rules: {
+    'unicorn/filename-case': 'off',
+    "n/no-unsupported-features/es-syntax": ["error", {
+      "version": ">=16.0.0",
+      "ignores": []
+  }]
+  }
+};
